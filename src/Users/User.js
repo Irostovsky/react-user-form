be9035e0174cc15199d8467760e2bed9d226094e
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2022-12-15 09:43:54 +0200

    Added a text for years.

--- src/Users/User.js
+++ src/Users/User.js
@@ -3,7 +3,7 @@ import styles from "./User.module.css";
 const User = (props) => {
   return (
     <div className={styles.user}>
-      {props.username} ({props.age})
+      {props.username} ({props.age} years old)
     </div>
   );
 };
